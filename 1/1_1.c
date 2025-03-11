0c2d8ca84ea625e407e8a955b4c65b3450742e64
YueqingDai001 <bp66290071@126.com>
2025-03-11 22:54:38 +0800

    5files day2

--- 1/1_1.c
+++ 1/1_1.c
@@ -3,9 +3,9 @@ int main (void)
 {
 	int dogs;//定义变量
 	
-	printf("���м�ֻ����\n");//��Ļ��ӡ 输出函数
-	scanf("%d",&dogs);//���̻�ȡ���� 输入函数
-	printf("��������%dֻ����\n",dogs);//%d ռλ����������ϱ��� 输出数字
+	printf("你有几只狗？\n");//输出函数
+	scanf("%d",&dogs);//输入函数
+	printf("可以的，你有%d 只狗狗\n");//输出
 	
 	return 0;
  } 
