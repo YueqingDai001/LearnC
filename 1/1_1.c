e839d710541b16ead8dc201de9b90842ff65f3e3
YueqingDai001 <bp66290071@126.com>
2025-03-28 16:23:59 +0800

    finish chapter 6

--- 1/1_1.c
+++ 1/1_1.c
@@ -5,7 +5,7 @@ int main (void)
 	
 	printf("how many dogs do you have？\n");//输出函数
 	scanf("%d",&dogs);//输入函数
-	printf("Yes you got %d dogs\n");//输出
+	printf("Yes you got %d dogs\n"，dogs);//输出
 	
 	return 0;
  } 
